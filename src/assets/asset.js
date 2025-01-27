901e3f25cd167e33a05142a072651e7d0ea29322
Pluggito <Pluggito@example.com>
2025-01-28 00:38:00 +0100

    SignUp and Login page commit

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -1,9 +1,11 @@
 import hero_image from './Home-Page.png'
 import dashboard_image from './Dashboard.png'
+import google_icon from './google.svg'
 
 
 
 export const assets = {
     hero_image,
-    dashboard_image
+    dashboard_image,
+    google_icon
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,35 @@
+import { useLocation } from "react-router-dom"
 
 
 
 
 const Footer = () => {
+
+    const location = useLocation();
+
     return (
-      <div className="flex flex-col justify-center items-center text-center mx-auto absolute left-0 right-0 bottom-2 w-auto ">
+      <div className={`flex flex-col justify-center items-center text-center mx-auto ${location.pathname === '/' ? "absolute left-0 right-0 bottom-2 w-auto " : ""}`}>
           {/*--- first section---- */}
-          <div>
-              <p className="text-lg">Want to share more memories? Keep uploading!</p>
-          </div>
+          {location.pathname === "/" && (
+        <div>
+          <p className="text-lg">Want to share more memories? Keep uploading!</p>
+        </div>
+      )}
   
           {/*-----second section---- */}
           <div>
-              <ul className="flex flex-row items-center justify-between gap-2 text-gray-600">
+              <ul className="flex flex-row items-center justify-between gap-3 text-gray-600">
                   <p className="cursor-pointer">Privacy Policy</p>
-                  <div className="w-0.5 h-3 bg-gray-600"></div>
+                  <hr className="w-0.5 h-3 bg-gray-600"/>
                   <p className="cursor-pointer">Terms of Service</p>
-                  <div className="w-0.5 h-3 bg-gray-600"></div>
+                  <hr className="w-0.5 h-3 bg-gray-600"/>
                   <p className="cursor-pointer">Cookie Policy</p>
               </ul>
-  
-          </div>
+            </div>
   
           {/*----third section----- */}
           <div>
-              <p className="text-gray-500">© 2025. <span className="text-[#c300f9]">Moooments</span> </p>
+              <p className="text-gray-500 text-sm">© 2025. <span className="text-[#c300f9]">Moooments</span> </p>
           </div>
         
   
