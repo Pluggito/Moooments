edbb96c6f496c259f2a602ce1c19434621a6bda8
Pluggito <Pluggito@example.com>
2025-01-30 12:01:44 +0100

    omo

--- src/pages/EventLink.jsx
+++ src/pages/EventLink.jsx
@@ -9,7 +9,8 @@ const EventLink = () => {
              <p className="text-2xl mb-2 font-semibold  w-full text-center">Place Your Event Link Here!</p>
              <div className="flex flex-col justify-center items-center gap-4 p-6 w-full bg-[#c300f9] rounded-lg">
              <input type="link" className="w-full sm:w-3/4 p-3 rounded-lg text-black hover:bg-slate-50 border border-gray-500 focus:outline-none bg-white"
-             placeholder="Event Link"/>
+             placeholder="Event Link" 
+             name="event-link"/>
             <button className="rounded sm:w-[101px] sm:h-[44px] w-1/2 font-bold hover:bg-gray-800 bg-black text-slate-100 hover:text-white transition-all duration-300
             cursor-pointer shadow-md p-2">Enter</button>
              </div>
